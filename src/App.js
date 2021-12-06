4bac7b5b5ba1743ddfef3e363e74f6290d8be5ce
kangkong <kang3530554@gmail.com>
2021-12-06 21:15:25 +0900

    start school idea festival

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const cors = require('cors')
+const io = require('socket.io')(server,{
+    cors : {
+        origin :"*",
+        credentials :true
+    }
+});
+
+
+io.on('connection', socket=>{
+    socket.on('message',({name,message}) => {
+        io.emit('message',({name, message}))
+    })
+})
+
+server.listen(4000, function(){
+    console.log('listening on port 4000');
+})
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
