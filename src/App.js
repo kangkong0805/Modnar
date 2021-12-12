405a3b4fb33b67e285dbf3540b72a10c4e34f432
kangkong <kang3530554@gmail.com>
2021-12-12 19:03:48 +0900

    start to make chat, header, join

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import CountUp, { useCountUp } from 'react-countup';
-import React from 'react';
+import React from 'react'
+import Chat from './Chat'
+import Header from './Header'
 
-const App = () => {
-  const countUpRef = React.useRef(null);
-  const { start, pauseResume, reset, update } = useCountUp({
-    ref: countUpRef,
-    start: 0,
-    end: 1234567,
-    delay: 1,
-    duration: 5,
-    onReset: () => console.log('Resetted!'),
-    onUpdate: () => console.log('Updated!'),
-    onPauseResume: () => console.log('Paused or resumed!'),
-    onStart: ({ pauseResume }) => console.log(pauseResume),
-    onEnd: ({ pauseResume }) => console.log(pauseResume),
-  });
+function App() {
   return (
-    <div>
-      <div ref={countUpRef} />
-      <button onClick={() => update(1234567)}>Start</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={pauseResume}>Pause/Resume</button>
-      <button onClick={() => update(2000)}>Update to 2000</button>
-    </div>
-  );
+    <>
+      <Header />
+      <Chat />
+    </>
+  )
 }
 
-export default App;
-// 기모띠
+export default App
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import react from "react";
+
+const Header = () => {
+    return(
+        <>
+            <header id="container">
+                <img src="" alt="logo"/>
+                <ul>
+                    <li>프로필</li>
+                    <li>테마</li>
+                </ul>
+            </header>
+        </>
+    )
+}
+
+export default Header;
