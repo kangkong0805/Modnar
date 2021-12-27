0dabf8ad1c4cf4b5a57ca88e5b77e9d0901df893
kangkong <kang3530554@gmail.com>
2021-12-27 23:50:20 +0900

    Upload page creation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './html/Home';
-import Join from './html/Join';
+import Dibs from './html/Dibs';
 import Upload from './html/Upload';
+import Error from './html/Error';
 
 function App() {
   return (
@@ -13,11 +14,52 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/join" element={<Join/>} />
-        <Route path="/Upload" element={<Upload/>} />
+        <Route path="/dibs" element={<Dibs/>} />
+        <Route path="/upload" element={<Upload/>} />
+        <Route path="*" element={<Error/>}/>
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+// import React from "react";
+// // import "./styles.css";
+
+// const useNotification = (title, options) => {
+//   if (!("Notification" in window)) {
+//     return;
+//   }
+
+//   const fireNotif = () => {
+//     /* 권한 요청 부분 */
+//     if (Notification.permission !== "granted") {
+//       Notification.requestPermission().then((permission) => {
+//         if (permission === "granted") {
+//           /* 권한을 요청받고 nofi를 생성해주는 부분 */
+//           new Notification(title, options);
+//         } else {
+//           return;
+//         }
+//       });
+//     } else {
+//       /* 권한이 있을때 바로 noti 생성해주는 부분 */
+//       new Notification(title, options);
+//     }
+//   };
+//   return fireNotif;
+// };
+
+// const App = () => {
+//   const triggerNotif = useNotification("tlqkf", {
+//     body: "tprtm"
+//   });
+//   return (
+//     <div className="App">
+//       <button onClick={triggerNotif}> Push notification </button>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/Html/Upload.js
+++ src/Html/Upload.js
@@ -1,42 +1,115 @@
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
 
-const Upload = () => {
-  const [inputs, setInputs] = useState({
-    contents: ""
-  });
-  const { contents } = inputs;
+function Upload() {
+  const [tags, setTags] = useState({
+    electronics: false,
+    fasion: false,
+    food: false,
+    free: false,
+    unknown: false
+  })
+  const { electronics, fasion, food, free, unknown } = tags;
+
+  const [file, setFile] = useState([]);
+  const [fileUrl, setFileUrl] = useState(null);
 
   const onChange = (e) => {
-    const { value, name } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
+    console.log(e.target.files.length);
+
+    for (let i = 0; i < e.target.files.length; i++) {
+      const reader = new FileReader();
+      reader.onloadend = (a) => {
+        setFile((file) => [...file, a.currentTarget.result]);
+      };
+      if (e.target.files && e.target.files[i])
+        reader.readAsDataURL(e.target.files[i]);
+    }
   };
 
-  const onSumbit = (e) => {
-    console.log({ contents });
+  const onClick = (e) => {
+    const { name } = e.target;
 
-  }
+    if(name == 'electronics'){
+      setTags({
+        ...tags,
+        electronics: !electronics
+      });
+    } else if(name == 'fasion'){
+      setTags({
+        ...tags,
+        fasion: !fasion
+      });
+    } else if(name == 'food'){
+      setTags({
+        ...tags,
+        food: !food
+      });
+    } else if(name == 'free'){
+      setTags({
+        ...tags,
+        free: !free
+      });
+    } else if(name == 'unknown'){
+      setTags({
+        ...tags,
+        unknown: !unknown
+      });
+    }
 
-  const onImage = (e) =>{
-    console.log(e);
+    for(let index in tags){
+      // console.log(tags[index]);
+    }
   }
 
   return (
     <>
       <Header />
-      <div className="post">
-        <label for="image_uploads">Choose images to upload (PNG, JPG)</label>
-        <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" multiple onChange={onImage}/>
-      </div>
-      <textarea name="contents" value={contents} onChange={onChange} />
-      <button onClick={onSumbit}>업로드</button>
-    </>
-  )
-}
+      <div className="upload">
+        <div id="component">
+          <h2>사진</h2>
+          <input type="file" multiple onChange={onChange} id="picture" />
+          <label htmlFor="picture">
+            <div id="pictureBox">
+              {file[0] ? (
+                <img id="preview" src={file[0]} alt="img" />
+              ) : (
+                <>
+                  <img id="nonePicture" src="grayLogo.png" />
+                  <p>사진을 선택해주세요.</p>
+                </>
+              )}
+            </div>
+          </label>
+          <input id="picture" type={"file"}></input>
+        </div>
 
+        <div id="title-component">
+          <h2>제목</h2>
+          <input type={"text"} id="title"></input>
+        </div>
+
+        <div id="component">
+          <h2>태그</h2>
+          <button className="tag" onClick={onClick} name="electronics" style={{ background: electronics ? "#f1bba4" : "#fff" }}>전자기기</button>
+          <button className="tag" onClick={onClick} name="fasion" style={{ background: fasion ? "#f1bba4" : "#fff" }}>패션</button>
+          <button className="tag" onClick={onClick} name="food" style={{ background: food ? "#f1bba4" : "#fff" }}>식품</button>
+          <button className="tag" onClick={onClick} name="free" style={{ background: free ? "#f1bba4" : "#fff" }}>무료나눔</button>
+          <button className="tag" onClick={onClick} name="unknown" style={{ background: unknown ? "#f1bba4" : "#fff" }}>미개봉</button>
+        </div>
+
+        <div id="component">
+          <textarea id="description" placeholder="설명"></textarea>
+        </div>
+
+        <div id="component" className="price">
+          <h2>가격</h2>
+          <input type={"text"} id="price"></input>
+          <h2 id="won">원</h2>
+        </div>
+        <input type="button" id="uploadBtn" value={"업로드"}></input>
+      </div>
+    </>
+  );
+}
 export default Upload;
