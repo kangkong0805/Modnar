7cbbee0ebe64d10456061923b6cf4407be3233a5
kangkong <kang3530554@gmail.com>
2021-12-28 23:10:10 +0900

    make footer

---
+++ src/Html/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div id="footer">
+      <p id="footer-text">
+        ⊙ for Gwangju Software Meister Highschool
+        <br></br>Brick Market
+      </p>
+    </div>
+  );
+}
