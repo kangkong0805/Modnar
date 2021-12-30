5f45ffa17739faca6bc555090031aa66ee8db4b1
kangkong <kang3530554@gmail.com>
2021-12-30 23:52:07 +0900

    start getting list

--- src/Html/Home.js
+++ src/Html/Home.js
@@ -2,42 +2,50 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../App.css';
 import Header from './Header';
+import Footer from './Footer';
 
 const Home = () => {
-  const [ip, setIp] = useState(''); // IP주소 변수 선언
 
-  function callback(data) {         // IP주소 값을 설정합니다.
-    setIp(data);
+  const [list, setList] = useState([]);
+
+  const Button = () => {
+    axios.get('/main')
+      .then(({ data }) => {
+        console.log(data[0]);
+        setList({
+          list: data[0][0][0]
+        })
+        console.log(list);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
-  useEffect(                        // 첫번째 렌더링을 다 마친 후 실행합니다.
-    () => {
-      axios.post('/api/ip',{
-          // url: '/api' + '/ip',
-          // method: 'post',
-          /**
-           * 개발 환경에서의 크로스 도메인 이슈를 해결하기 위한 코드로
-           * 운영 환경에 배포할 경우에는 15~16행을 주석 처리합니다.
-           * 
-           * ※크로스 도메인 이슈: 브라우저에서 다른 도메인으로 URL 요청을 하는 경우 나타나는 보안문제
-           */
-          // baseURL: 'http://localhost:8080',
-          // withCredentials: true,
-        }
-      ).then(function (response) {
-        console.log(response);
-        callback(response.data);      // 클라이언트의 IP주소를 반환
-      });
-    }, []
-  );
   return (
     <>
       <Header />
       <main>
-        이 기기의 IP주소는 {ip}입니다.<br/>
+      <button onClick={Button}>버튼</button>
+        <ul>
+          {/* {list.map((index, num) => {
+            return (
+              <>
+                <li>{index.title}</li>
+              </>
+            )
+          })} */}
+
+          {/* {
+            {list.title}
+          } */}
+        </ul>
+
+
       </main>
+      <Footer />
     </>
   );
 };
 
-export default Home;
+export default Home
