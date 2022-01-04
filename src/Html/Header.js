538ec52f762578456b06ac62bf94cd68a5314a3e
kangkong <kang3530554@gmail.com>
2022-01-05 08:19:25 +0900

    Fix and add function

--- src/Html/Header.js
+++ src/Html/Header.js
@@ -1,33 +1,92 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-import Login from './Login';
+import Login from './account/Login';
+import Join from './account/Join';
+import ChangePW from './account/ChangePW';
+import Profile from './Profile';
+import axios from 'axios';
 
 const Header = () => {
-  const [isLogin, setLogin] = useState(false);
-  const [isJoin, setJoin] = useState(false);
+
+  const [isLogin, setLogin] = useState(false);         // 로그인 컴포넌트 
+  const [isProfile, setProfile] = useState(false);     // 프로필 컴포넌트
+  const [isClick, setClick] = useState(false);         // 첫 렌더링 시 프로필 클릭되었는지 체크
+  const [isJoin, setJoin] = useState(false);           // 회원가입 컴포넌트
+  const [isFindPW, setFindPW] = useState(false);       // 비밀번호 찾기 컴포넌트
+  const [loginCheck, setLoginCheck] = useState(() => JSON.parse(window.localStorage.getItem("loginCheck")) || false); // 로그인 되어 있는지 체크
+
+  useEffect(() => {
+    window.localStorage.setItem("loginCheck", JSON.stringify(loginCheck));
+  }, [loginCheck])
+
+  const onProfile = () => {
+    triggerNotif(); //  알림 띄우기
+    setClick(true);
+    setProfile(!isProfile);
+  }
+
+  const useNotification = (title, options) => {
+    if (!("Notification" in window)) {
+      return;
+    }
+
+    const fireNotif = () => {
+      /* 권한 요청 부분 */
+      if (Notification.permission !== "granted") {
+        Notification.requestPermission().then((permission) => {
+          if (permission === "granted") {
+            /* 권한을 요청받고 nofi를 생성해주는 부분 */
+            new Notification(title, options);
+          } else {
+            return;
+          }
+        });
+      } else {
+        /* 권한이 있을때 바로 noti 생성해주는 부분 */
+        new Notification(title, options);
+      }
+    };
+    return fireNotif;
+  };
+
+  const triggerNotif = useNotification("Profile", { // 제목
+    body: "this is profile " + !isProfile  // 본문
+  });
+
+  const logout = () => {
+    axios.post('/logout')
+      .then((response) => {
+        console.log(response);
+        setLoginCheck(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
   return (
     <>
-      <font>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Ruda&display=swap" rel="stylesheet" />
-      </font>
-
-      <header>
+      <header>  {/* 헤더바 */}
         <div>
-          {/* <Link to="/"><img src="" alt="Logo"></img></Link> */}
-          <h1>Logo</h1>
+          <Link to="/"><img src="img/logo.png" alt="logo" /></Link>  {/* 로고 */}
         </div>
         <nav>
           <ul>
-            <li tapindex="0"><h3>로그인</h3></li>
-            <li><h3>프로필</h3></li>
-            <li><h3>찜</h3></li>
-            {isLogin && <Login onClose={() => setLogin(false)} />}
+            {loginCheck ? (
+              <li onClick={logout} tapindex="0" style={{ color: isLogin ? "#000" : "#727272" }}><h3>로그아웃</h3></li>
+            ) : (
+              <li onClick={() => setLogin(true)} tapindex="0" style={{ color: isLogin ? "#000" : "#727272" }}><h3>로그인</h3></li>)}
+
+            <li onClick={onProfile} style={{ color: isProfile ? "#000" : "#727272" }}><h3>프로필</h3></li>
+            <Link to="/dibs"><li style={{ color: "#727272" }}><h3>찜</h3></li></Link>
+
+            {isLogin && <Login onClose={() => setLogin(false)} join={() => setJoin(true)} findPW={() => setFindPW(true)} name={() => setLoginCheck(true)} />} {/* 로그인 창 */}
+            {isJoin && <Join onClose={() => setJoin(false)} />}  {/* 회원가입 창 */}
+            {isFindPW && <ChangePW onClose={() => setFindPW(false)} />} {/* 비밀번호 찾기 창 */}
+            <Profile place={isProfile} start={isClick} /> {/* 프로필 창 */}
+
           </ul>
         </nav>
-
       </header>
     </>
   );
